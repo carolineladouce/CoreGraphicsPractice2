49aa19da513813cecb5c0ef4f19f4810cf565388
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2021-10-05 11:08:42 -0400

    Initial Commit

---
+++ CoreGraphicsPractice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreGraphicsPractice2
+//
+//  Created by Caroline LaDouce on 10/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
