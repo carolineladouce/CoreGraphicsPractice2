0de6032a5810a5bcebdbc4af077db7e2bcede5b6
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2021-10-25 16:58:13 -0400

    Setup initial constraints and drew basic sample circle

--- CoreGraphicsPatterns1/BackgroundPattern1.swift
+++ CoreGraphicsPatterns1/BackgroundPattern1.swift
@@ -18,15 +18,77 @@ class BackgroundPattern1: UIView {
     */
     
     var lineColor: UIColor = .black
+    var fillColor: UIColor = .orange
     var patternSize: CGFloat = 300
     
+    
     override func draw(_ rect: CGRect) {
-        guard let context = UIGraphicsGetCurrentContext() else {
-            fatalError("Failed to get current context.")
-        }
+
+//        guard let context = UIGraphicsGetCurrentContext() else {
+//            fatalError("\(#function):\(#line) Failed to get current context.")
+//        }
+//
+//        let drawingSize = CGSize(width: patternSize, height: patternSize)
+//
+//        UIGraphicsBeginImageContextWithOptions(drawingSize, true, 0.0)
+//
+//        guard let drawingContext = UIGraphicsGetCurrentContext() else {
+//            fatalError("\(#function):\(#line) Failed to get current context.")
+//        }
+
+//        UIColor.purple.setFill()
+//        drawingContext.fill(CGRect(x: 0, y: 0, width: drawingSize.width, height: drawingSize.height))
         
-        context.setFillColor(lineColor.cgColor)
-        context.fill(rect)
+        let innerOvalPath = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: patternSize, height: patternSize))
+        
+        innerOvalPath.lineWidth = 4.0
+        fillColor.setStroke()
+        innerOvalPath.stroke()
+//
+//        guard let image = UIGraphicsGetImageFromCurrentImageContext() else { fatalError("""
+//    \(#function):\(#line) Failed to \
+//    get an image from current context.
+//    """)
+//        }
+//        UIGraphicsEndImageContext()
+//
+//        UIColor(patternImage: image).setFill()
+//        context.fill(rect)
+//
+        
+        
+        
+//        guard let context = UIGraphicsGetCurrentContext() else {
+//            fatalError("Failed to get current context.")
+//        }
+//
+//        context.setFillColor(fillColor.cgColor)
+//        context.fill(rect)
+//
+//        let drawSize = CGSize(width: patternSize, height: patternSize)
+//
+//        UIGraphicsBeginImageContextWithOptions(drawSize, true, 0.0)
+//
+//        guard let drawingContext = UIGraphicsGetCurrentContext() else {
+//            fatalError("Failed to get current context.")
+//        }
+//
+//
+//        drawingContext.fill(CGRect(x: 0, y: 0, width: drawSize.width, height: drawSize.height))
+//
+//        let innerOvalPath = UIBezierPath(
+//            ovalIn: CGRect(x: 50, y: 50, width: 10, height: 15)
+//        )
+//        innerOvalPath.lineWidth = 4.0
+//
+//        lineColor.setStroke()
+//        innerOvalPath.stroke()
+        
+        
+        
+//        UIColor(patternImage: drawingContext).setFill()
+//        context.fill(rect)
+          
     }
 
 }
--- CoreGraphicsPatterns1/MainViewController.swift
+++ CoreGraphicsPatterns1/MainViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    let backgroundPattern1 = BackgroundPattern1()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,6 +18,31 @@ class MainViewController: UIViewController {
         let view = UIView()
         view.backgroundColor = .white
         
+        
+//        let backgroundPattern1 = BackgroundPattern1(frame: self.view.bounds)
+        
+        view.addSubview(backgroundPattern1)
+        
+//        backgroundPattern1.contentMode = UIView.ContentMode.scaleAspectFill
+        
+        backgroundPattern1.translatesAutoresizingMaskIntoConstraints = false
+        backgroundPattern1.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        backgroundPattern1.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        backgroundPattern1.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        backgroundPattern1.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+        
+//        backgroundPattern1.translatesAutoresizingMaskIntoConstraints = false
+//        backgroundPattern1.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+//        backgroundPattern1.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+//        backgroundPattern1.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+//        backgroundPattern1.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+//
+//        backgroundPattern1.frame = self.view.safeAreaLayoutGuide.layoutFrame
+    
+//        backgroundPattern1.frame = view.frame
+        
+       
         self.view = view
     }
 
